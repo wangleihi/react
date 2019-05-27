b20e0ea0626e7f6aa8e666566d42c5b504aad341
Laney_wang <coding@coding.ide>
2019-05-27 20:19:17 +0800

    init

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    const element = <h1>Hello, world!</h1>;
+    ReactDOM.render(
+    element,
+    document.getElementById('example')
+);
+  );
+}
+
+export default App;
